22cc2c605a4d7acef917a615020e9f7f13ffd2da
kpekala <kgpekala@gmail.com>
2020-12-15 22:56:11 +0100

    lab3: added working stoer wagner algorithm

--- lab3/main.py
+++ lab3/main.py
@@ -1,11 +1,12 @@
 # https://faliszew.github.io/algograf/lab3
 
+
 from dimacs import loadWeightedGraph
 from queue import PriorityQueue
 from flow_method import *
 from utils import *
+from stoer_wagner import *
 
-(V,L) = loadWeightedGraph("graphs/grid100x100")
-graph = matrix_graph(V, L, True)
-
-print(connectivity(graph))
+(V,L) = loadWeightedGraph("graphs/clique200")
+graph = nodesFromEdges(L, V)
+print(stoerWagner(graph))
--- lab3/stoer_wagner.py
+++ lab3/stoer_wagner.py
@@ -1,7 +1,11 @@
+from queue import PriorityQueue
+import math
+
 class Node:
     def __init__(self):
         self.edges = {}
         self.disabled = False
+        self.processed = False
 
     def addEdge(self, to, weight):
         self.edges[to] = self.edges.get(to, 0) + weight
@@ -11,22 +15,61 @@ class Node:
 
     def disable(self):
         self.disabled = True
-        self.edges = []
+        self.edges = {}
+
+def notDisabled(graph):
+    return [v for v in graph if not v.disabled]
+
 
 
 def nodesFromEdges(edges, n):
     nodes = [Node() for i in range(n)]
     for (x, y, c) in edges:
-        nodes[x].addEdge(y, c)
-        nodes[y].addEdge(x, c)
+        nodes[x-1].addEdge(y-1, c)
+        nodes[y-1].addEdge(x-1, c)
+    return nodes
+
+def cleanGraph(graph):
+  for u in graph:
+    u.processed = False
+
+def minimumCutPhase(graph):
+    cleanGraph(graph)
+    n = len(notDisabled(graph))
+    S = []
+    weights = [0] * len(graph)
+    queue = PriorityQueue()
+    queue.put((0,0))
+    while len(S) < n: #and not queue.empty():
+      c, v = queue.get()
+      if graph[v].processed or graph[v].disabled:
+        continue
+      S.append(v)
+      graph[v].processed = True
+      #aktualizacja sum wag
+      for u in list(graph[v].edges):
+        weight = graph[v].edges.get(u)
+        weights[u] += weight
+        queue.put((-weights[u], u))
+
+    if len(S) >= 2:
+      mergeVertices(graph, S[-2], S[-1])
+    return weights[S[-1]]
+
+def stoerWagner(graph):
+  res = math.inf
+  while len(notDisabled(graph)) > 2:
+    res = min(res, minimumCutPhase(graph))
+  return res
 
 
-def mergeVertices(G, x, y):
-    for v in list(G[y].edges):
+
+def mergeVertices(graph, x, y):
+    for v in list(graph[y].edges):
         if v == x:
-            G[x].delEdge(y)
+            graph[x].delEdge(y)
         else:
-            weight = G[y].edges[v]
-            G[x].addEdge(v, weight)
-            G[v].addEdge(x, weight)
-    G[y].disable()
+            weight = graph[y].edges[v]
+            graph[x].addEdge(v, weight)
+            graph[v].addEdge(x, weight)
+    graph[y].disable()
